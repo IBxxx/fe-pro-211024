c2622ea2e94b174d0a368b640d81df00285e0655
IBxxx <xxx566890@gmail.com>
2024-11-26 19:25:22 +0100

    fe-pro-211024

---
+++ 07/hw/game.js
@@ -0,0 +1,8 @@
+function starGame(){
+    let option = ['камень', 'ножницы', 'бумага'];
+    let cjmputerChoise = options[math.floor(math.radom()*3)];
+    let userchoise = promt('напишите свой выбор камень, ножницы, бумага').trim().toLowerCase();
+    if(!options.includes(userChoise)){
+        alert()
+    }
+}
